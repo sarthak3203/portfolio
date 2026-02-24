18488fe8e557ea933e0bd60fe405a7fc836c34b2
sarthak3203 <sarthak3203@gmail.com>
2026-02-24 11:02:53 +0530

    added analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"; // 👈 add this
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Analytics /> {/* 👈 add this at the bottom inside body */}
       </body>
     </html>
   );
